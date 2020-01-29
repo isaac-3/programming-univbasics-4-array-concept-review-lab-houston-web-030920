42c9ec9a54e99eba9378e1b02802e6beb9bb4472
isaac chavez <isaacgc0596@gmail.com>
2020-01-29 20:38:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max = 0
+  max = array[0]
   array.length.times { |index|
     if array[index] > max
       x = array[index]
